492f944ee2885fc9c812ae682bc1c96b3f73abfc
Eldar Dzheparov <abduldzhabbar@gmail.com>
2024-02-24 20:38:33 +0300

    fix scenario.dart

--- lib/worlds/game_engine/loader/models/scenario.dart
+++ lib/worlds/game_engine/loader/models/scenario.dart
@@ -4,12 +4,21 @@ part 'scenario.g.dart';
 
 @JsonSerializable(createToJson: false)
 class Scenario {
-  Scenario({
-    required this.kind,
-    required this.sound,
-    required this.startFrom,
-    required this.loud,
-  });
+  Scenario(
+      {required this.kind,
+      required this.sound,
+      required this.startFrom,
+      required this.loud,
+      required this.actor,
+      required this.path,
+      required this.grub,
+      required this.checkpoint,
+      required this.speed,
+      required this.delay,
+      required this.trash,
+      required this.point,
+      required this.angle,
+      required this.addBonuses});
 
   factory Scenario.fromJson(Map<String, dynamic> json) =>
       _$ScenarioFromJson(json);
@@ -20,4 +29,15 @@ class Scenario {
   @JsonKey(name: 'startfrom')
   String? startFrom;
   String? loud;
+  String? actor;
+  String? path;
+  String? grub;
+  String? checkpoint;
+  String? speed;
+  String? delay;
+  String? trash;
+  String? point;
+  String? angle;
+  @JsonKey(name: 'addbonuses')
+  String? addBonuses;
 }
