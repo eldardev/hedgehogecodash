9ac8e2922e03b3e106660336b3eb702675d53715
Eldar Dzheparov <abduldzhabbar@gmail.com>
2024-02-29 17:25:01 +0300

    rename menu button to play button

--- lib/worlds/menu/menu_world.dart
+++ lib/worlds/menu/menu_world.dart
@@ -4,7 +4,7 @@ import 'package:urchin/worlds/common/common_world.dart';
 import 'package:urchin/worlds/menu/components/flower_component.dart';
 import 'package:urchin/worlds/menu/components/level_builder_button.dart';
 import 'package:urchin/worlds/menu/components/menu_background.dart';
-import 'package:urchin/worlds/menu/components/menu_button.dart';
+import 'package:urchin/worlds/menu/components/play_button.dart';
 
 @singleton
 class MenuWorld extends CommonWorld {
